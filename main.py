b3f33b56f69249ba160f3a98b3917e2fce98e42d
Wojciech Panfil <wojtek.panfil@wp.pl>
2022-05-21 18:05:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from game import Game
 
-my_game = Game()
-if my_game.setup():
-    my_game.play()
-else:
-    print("INFO: Error related to server connection\n")
+
+def main():
+    my_game = Game()
+    if my_game.setup():
+        my_game.play()
+    else:
+        print("INFO: Error related to server connection\n")
+
+
+if __name__ == "__main__":
+    main()
